01dab55c5c9aba50b07b5400aa9ea70828ec20cf
Mia <68258126+miaadev@users.noreply.github.com>
2026-05-23 19:51:01 +1200

    Init

---
+++ Starlight/StarlightFormat.cs
@@ -0,0 +1,20 @@
+﻿namespace Starlight
+{
+    public class StarlightFormat
+    {
+        public string AppName { get; set; }
+        public string AppVersion { get; set; }
+        public string CrashMessage { get; set; }
+        public string CrashType { get; set; }
+        public string CrashLogPath { get; set; }
+        public string CrashLogFile { get; set; }
+    }
+
+    public class StarlightViewerFormat
+    {
+        public StarlightFormat starlight { get; set; }
+        public string GUID { get; set; }
+        public string LogContent { get; set; }
+
+    }
+}
